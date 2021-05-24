a9922af3f9f9004d2a9508032e798bd99d6e8e5d
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-05-24 07:38:44 +0300

    revisions in index (html, js)

--- index.js
+++ index.js
@@ -83,19 +83,48 @@ class CountdownTimer {
     startTimer = () => {
         this.timerId = setInterval(this.reflectTime, 1000);
     }
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate.getTime();
+    this.days = document.querySelector(`${this.selector} span[data-value="days"]`);
+    this.hours = document.querySelector(`${this.selector} span[data-value="hours"]`);
+    this.mins = document.querySelector(`${this.selector} span[data-value="mins"]`);
+    this.secs = document.querySelector(`${this.selector} span[data-value="secs"]`);
+    this.start();
+  }
+
+  timerUpdate(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24))
+      .toString()
+      .padStart(2, "0");
+
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      .toString()
+      .padStart(2, "0");
+
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+      .toString()
+      .padStart(2, "0");
+
+    const secs = Math.floor((time % (1000 * 60)) / 1000)
+      .toString()
+      .padStart(2, "0");
+
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+
+  start() {
+    setInterval(() => {
+      const delta = this.targetDate - Date.now();
+      this.timerUpdate(delta);
+    }, 1000);
+  }
 }
 
-//Timer 1 example
-const countdownTimer = new CountdownTimer({
-    selector: "#timer-1",
-    targetDate: new Date(new Date().getTime() + 20000)
-});
-
-countdownTimer.reflectInitialDate();
-countdownTimer.startTimer();
-
-//Timer 2 example
-const countdownTimer1 = new CountdownTimer({
-    selector: "#timer-1",
-    targetDate: new Date("Oct 17, 2019")
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("May 28, 2021"),
 });
